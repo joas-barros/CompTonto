ea819e749df608e94d12f079f9ec7f8db8596245
joas-barros <joas.barros14@hotmail.com>
2025-10-14 00:03:57 -0300

    Update CMake configuration and add initial source files for Tonto Compiler

---
+++ Src/tokens.h
@@ -0,0 +1,22 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// Constantes para os tipos de tokens
+enum Token {
+    NUMERO = 1,
+    ESTERIOTIPO_CLASSE,
+    ESTERIOTIPO_RELACAO,
+    PALAVRA_RESERVADA,
+    SIMBOLO,
+    NOME_DE_CLASSE,
+    NOME_DE_RELACAO,
+    NOME_DE_INSTANCIA,
+    DADO_NATIVO,
+    NOVO_TIPO,
+    META_ATRIBUTO,
+    NAO_IDENTIFICADO
+};
+
+
+
+#endif 
