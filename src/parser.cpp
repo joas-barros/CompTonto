5676471a133f9408fdedb0a44129297bb30ff493
joas-barros <joas.barros14@hotmail.com>
2025-10-15 21:09:39 -0300

    refactor: folder struture

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,10 +4,18 @@ void Parser::start() {
     lookahead = lexer.yylex();
     while (lookahead != 0) { // 0 indica EOF
         string name = lexer.YYText();
-        int line = lexer.lineno;
+        int line = lexer.lineno();
         symbolTable.addSymbol(name, lookahead, line);
         lookahead = lexer.yylex();
     }
     generateSummary();
     exportJson();
+}
+
+void Parser::generateSummary() {
+    // Implementar a geração do resumo
+}
+
+void Parser::exportJson() {
+    // Implementar a exportação para JSON
 }
